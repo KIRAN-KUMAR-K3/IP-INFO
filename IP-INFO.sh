75844d9703dcfab9df5e19b5053d8f2395c8befe
KIRAN KUMAR K <137183954+KIRAN-KUMAR-K3@users.noreply.github.com>
2023-11-15 03:23:57 +0530

    Update IP-INFO.sh

--- IP-INFO.sh
+++ IP-INFO.sh
@@ -1,34 +1,16 @@
 #!/bin/bash
 
-# Function to get IP information
-get_ip_info() {
-    local ip_address=$1
-    local api_url="https://ipinfo.io/$ip_address/json"
+echo "Enter the IP address you want to trace:"
+read ip_address
 
-    # Use curl to make a GET request to the API and store the response in a variable
-    local response=$(curl -s "$api_url")
+# Make a request to ipinfo.io to get information about the provided IP address
+response=$(curl -s "https://ipinfo.io/${ip_address}")
 
-    # Check if the request was successful
-    if [ $? -eq 0 ]; then
-        # Print the details of the IP address
-        echo "Details for IP Address: $ip_address"
-        echo "$response" | jq    # Requires jq for pretty printing JSON, you can install it with 'sudo apt-get install jq'
-    else
-        echo "Error fetching details for IP Address: $ip_address"
-    fi
-}
-
-# Prompt the user to choose between fetching their own IP or entering one
-read -p "Do you want information for your own IP? (y/n): " choice
-
-if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
-    # Use curl to fetch the public IP address
-    ip_address=$(curl -s ifconfig.me)
-
-    # Display the public IP address
-    echo "Your public IP address: $ip_address"
+# Check if the request was successful
+if [ $? -eq 0 ]; then
+    # Display the information
+    echo "Information about IP address $ip_address:"
+    echo "$response" | jq    # If you don't have jq installed, you can remove this line or install jq for better formatting
 else
-    # Prompt the user to enter an IP address
-    read -p "Enter an IP address: " user_ip
-    get_ip_info "$user_ip"
+    echo "Error: Unable to fetch information. Please check your internet connection or try again later."
 fi
