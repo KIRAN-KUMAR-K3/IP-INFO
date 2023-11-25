6f95c7ad07f895af8c8396bc3e804c2a627ffc9e
KIRAN-KUMAR-K3 <18kirankumar.k03@gmail.com>
2023-11-25 15:33:12 +0530

    new changes

--- IP-INFO.sh
+++ IP-INFO.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-echo "Enter the IP address you want to trace:"
-read ip_address
+get_ip_details() {
+    ip_address=$1
+    api_url="https://ipinfo.io/${ip_address}/json"
+    
+    response=$(curl -s "$api_url")
 
-# Make a request to ipinfo.io to get information about the provided IP address
-response=$(curl -s "https://ipinfo.io/${ip_address}")
+    if [ "$(echo "$response" | jq '.ip' 2>/dev/null)" != "null" ]; then
+        echo "IP Address: $(echo "$response" | jq -r '.ip')"
+        echo "Hostname: $(echo "$response" | jq -r '.hostname // "N/A")"
+        echo "City: $(echo "$response" | jq -r '.city // "N/A")"
+        echo "Region: $(echo "$response" | jq -r '.region // "N/A")"
+        echo "Country: $(echo "$response" | jq -r '.country // "N/A")"
+        echo "Location: $(echo "$response" | jq -r '.loc // "N/A")"
+        echo "Organization: $(echo "$response" | jq -r '.org // "N/A")"
+    else
+        echo "Error: Unable to fetch details for ${ip_address}"
+    fi
+}
 
-# Check if the request was successful
-if [ $? -eq 0 ]; then
-    # Display the information
-    echo "Information about IP address $ip_address:"
-    echo "$response" | jq    # If you don't have jq installed, you can remove this line or install jq for better formatting
-else
-    echo "Error: Unable to fetch information. Please check your internet connection or try again later."
-fi
+read -p "Enter the IP address to look up: " ip_address_to_lookup
+get_ip_details "$ip_address_to_lookup"
